f9ab70a3bc12d67d69b27150c67950d3b3a7ef36
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-10-23 18:32:42 -0300

    Aula - 5 - 53 - Criando Componente If - Renderização Condicional (If)

--- frontend/src/todo/todoForm.js
+++ frontend/src/todo/todoForm.js
@@ -6,10 +6,8 @@ export default props => (
     <div className="row">
 
         <Grid cols='12 9 10'>
-
             <input type="text" className="form-control" id="description" placeholder="Adicione tarefas"/>
         </Grid>
-
 
         <Grid cols='12 3 2'>
             <Button style='primary' icon="plus"/>
