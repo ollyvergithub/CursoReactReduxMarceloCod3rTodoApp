f7e9146c485359fe8883936e33f4cdbcf455d0d8
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-10-23 17:35:58 -0300

    Aula - 5 - 51 - Estrutura do Formulário

--- frontend/src/todo/todoForm.js
+++ frontend/src/todo/todoForm.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 export default props => (
-    <div>
-        <h1>Form</h1>
+    <div className="row">
+
+            <div className="col-12 col-md-9 col-lg-10">
+                <input type="text" className="form-control" id="description" placeholder="Adicione tarefas"/>
+            </div>
+            <div className="col-12 col-md-3 col-lg-2">
+                <button type="submit" className="btn btn-primary"><i className="fa fa-plus"></i></button>
+            </div>
+
     </div>
 
 );
