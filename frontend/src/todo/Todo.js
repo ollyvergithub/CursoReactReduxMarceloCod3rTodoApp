109f9de046300e16021c73bb49b6ace810d7b5de
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-10-24 09:52:42 -0300

    Aula - 5 - 56 - Evento Adicionar (Integração Backend) - COM FETCH

--- frontend/src/todo/Todo.js
+++ frontend/src/todo/Todo.js
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import PageHeader from '../template/pageHeader';
 import TodoForm from '../todo/todoForm';
 import TodoList from '../todo/todoList';
+
+const URL = 'http://localhost:3003/api/todos';
 
 class Todo extends React.Component{
     constructor(props){
@@ -17,6 +20,28 @@ class Todo extends React.Component{
     handleAdd(){
         console.log(this);
         console.log(this.state.description);
+        const DESCRIPTION = this.state.description;
+
+        const requestInfo = {
+            method:'POST',
+            body:JSON.stringify({description: DESCRIPTION}),
+            headers: new Headers({
+                'Content-type':'application/json'
+            })
+        };
+
+        fetch(URL, requestInfo)
+            .then(response=>{
+                if(response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("não foi possível inserir na API");
+                }
+            })
+            .then(resposta=>{
+                console.log(resposta)
+            })
+
     }
 
     render() {
