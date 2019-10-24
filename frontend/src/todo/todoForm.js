04de3f4a6491dde65305acad13943590838e09c8
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-10-24 13:02:16 -0300

    Aula - 5 - 61 - Adicionar Teclas de Atalhos

--- frontend/src/todo/todoForm.js
+++ frontend/src/todo/todoForm.js
@@ -4,11 +4,29 @@ import Button from '../template/Button';
 
 export default props => {
 
+    const keyHandler = (e) =>{
+        if (e.key === 'Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd();
+        }else if (e.key === 'Escape'){
+            props.handleClear();
+        }
+    }
+
     return(
         <div className="row">
 
             <Grid cols='12 9 10'>
-                <input type="text" className="form-control" id="description" value={props.description} onChange={props.handleChange} placeholder="Adicione tarefas"/>
+                <h6 className='mt-2 mb-4'>Adicione tarefas com a tecla <code>Enter </code>, pesquise com <code>Shif+Enter</code> e limpe a pesquisa com <code>Esc</code></h6>
+            </Grid>
+
+            <Grid cols='12 9 10'>
+                <input type="text"
+                       className="form-control"
+                       id="description"
+                       onKeyUp={keyHandler}
+                       value={props.description}
+                       onChange={props.handleChange}
+                       placeholder="Adicione tarefas"/>
             </Grid>
 
             <Grid cols='12 3 2'>
